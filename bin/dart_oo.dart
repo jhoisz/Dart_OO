ffeb4334e96c83950eeeffcf9e168c83326c93c6
jhoisz <jvrda@ufpi.edu.br>
2022-12-15 20:32:20 -0300

    Projeto Inicial

---
+++ bin/dart_oo.dart
@@ -0,0 +1,5 @@
+import 'package:dart_oo/dart_oo.dart' as dart_oo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_oo.calculate()}!');
+}
---
+++ test/dart_oo_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_oo/dart_oo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
